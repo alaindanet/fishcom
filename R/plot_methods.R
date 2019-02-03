ed1fcca14f6bf7421ad3b378dae93d2d7e539643
Alain D <alain.danet@caramail.fr>
2019-02-03 16:12:14 +0100

    Set plot method for temporal networks

--- R/plot_methods.R
+++ R/plot_methods.R
@@ -1,3 +1,41 @@
+#' Generate ggraph with a common layout
+#'
+#'  
+#' @param net: data.frame describing edges 
+#' @param glay a layout objet created by create_layout
+#' @param title chr
+#'
+#' @return a ggraph object
+set_layout_graph <- function (net, glay, title = NULL) {
+
+      net <- graph_from_data_frame(net)
+      # get node coord from the general:
+      node_names <- attr(V(net), "name")
+      coord <- filter(glay, name %in% node_names) %>%
+	select(x, y, name)
+
+      # local layout:
+      llay <- create_layout(net, layout = "kk")
+      saved_graph <- attr(llay, "graph")
+      saved_circular_attr <- attr(llay, "circular")
+      saved_class <- class(llay)
+      llay <- select(llay, -x, -y) %>%
+	left_join(., coord, by = "name") %>%
+	select(x, y, everything())
+      attr(llay, "graph") <- saved_graph
+      attr(llay, "circular") <- saved_circular_attr
+      class(llay) <- saved_class 
+
+      p <- ggraph(llay) + 
+	geom_edge_fan(aes(alpha = ..index..), show.legend = FALSE) + 
+	geom_node_point()
+
+      if(!is.null(title)) {
+	p <- p + labs(title = title)
+      }
+      p + theme_graph(foreground = 'steelblue', fg_text_colour = 'white')
+}
+
 #' Personal plot theme
 #'
 #' @details This theme was design from the theme developped in the hrbrthemes
