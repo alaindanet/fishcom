b540802b78979296b055437787df4325896473a3
Alain DANET <danet@mesu0.ib0.xa.dsi.upmc.fr>
2019-03-04 17:53:07 +0100

    Replace littler by Rscript

---
+++ run/install.R
@@ -0,0 +1,27 @@
+#!/usr/bin/Rscript 
+argv <- commandArgs(trailingOnly=TRUE)
+
+if (is.null(argv) | length(argv) < 1) {
+  cat("Usage: installr.r pkg1 [pkg2 pkg3 ...]\n")
+  q()
+}
+
+## adjust as necessary, see help('download.packages')
+repos <- "https://cran.rstudio.com"
+
+## this makes sense on Debian where no packages touch /usr/local
+lib.loc <- "~/.local/share/R-3.5.2/library/"
+
+## Remove from the list already installed packages
+already_installed <- installed.packages()[, "Package"]
+argv <- argv[which(!argv %in% already_installed)]
+
+if (is.null(argv) | length(argv) < 1) {
+  cat("All the packages were already installed\n")
+} else {
+  install.packages(argv, lib.loc, repos)
+  cat("The following packages have been installed:\n", argv, ".\n")
+}
+
+
+
