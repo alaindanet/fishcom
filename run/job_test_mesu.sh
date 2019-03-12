953ac13cf1569fb1fd8f4d8b3f2e1bae54fad2b8
Alain DANET <danet@mesu2.ib0.xa.dsi.upmc.fr>
2019-03-12 22:05:52 +0100

    Test of cluster works

--- run/job_test_mesu.sh
+++ run/job_test_mesu.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
-#PBS -l select=1:ncpus=1
-#PBS -l walltime=60:00:00
-#PBS -N mytestjob
-#PBS -j oe
-echo "It works" > result.txt
+#PBS -q alpha 
+#PBS -l select=1:ncpus=3
+#PBS -l walltime=00:10:00
+#PBS -N test_future
 
+# Go to my place
+cd $PBS_O_WORKDIR
+
+
+# Load R version of the cluster
+module load R/3.4.3
+
+## Which version is used?
+which R | echo 
+which Rscript | echo 
+
+R_LIBS_USER="$R_LIBS_USER:$HOME/.local/share/R-3.4.3/library/"
+export R_LIBS_USER
+
+# Launch the good guys: 
+./A_test_future.R "PBS"
