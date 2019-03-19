55b490cdc5cb9f7fefe137fe6b503c143e554ed1
Alain D <alain.danet@caramail.fr>
2019-03-19 11:09:48 +0100

    compute average_species_overlap

--- tests/testthat/test-diet_overlap.R
+++ tests/testthat/test-diet_overlap.R
@@ -20,13 +20,35 @@ test_that("overlap return the good values", {
   expect_equal(round(res, 2), .82)
 })
 
-test_that("Overlap for matrix works", {
-  sp1 <- c(0, 1, 0, 1)
-  sp2 <- c(1, 0, 1, 0)
-  sp3 <- c(1, 1, 1, 0)
-  adj <- cbind(sp1, sp2, sp3)
+sp1 <- c(0, 1, 0, 1)
+sp2 <- c(1, 0, 1, 0)
+sp3 <- c(1, 1, 1, 0)
+adj <- cbind(sp1, sp2, sp3)
 
-  res <- compute_diet_overlap(adj)
+res <- compute_diet_overlap(adj)
+
+test_that("Overlap for matrix works", {
   expected <- c(Osp1_sp2 = 0, Osp1_sp3 = .41, Osp2_sp3 = .82)
-  expect_equal(round(res, 2), expected)
+  expected_mat <- matrix(1, nrow = ncol(adj), ncol = ncol(adj),
+    dimnames = list(colnames(adj), colnames(adj)))
+  expected_mat[upper.tri(expected_mat, diag = FALSE)] <- expected 
+  expected_mat[lower.tri(expected_mat, diag = FALSE)] <-
+    expected_mat[upper.tri(expected_mat, diag = FALSE)]
+
+  expect_equal(round(res, 2), expected_mat)
+})
+
+test_that("We can get average overlap", {
+
+  diag(res) <- NA
+  newres <- matrix(
+    res[which(!is.na(res))],
+    nrow = nrow(res) - 1,
+    ncol = ncol(res),
+    byrow = FALSE
+  )
+  colnames(newres) <- colnames(res)
+
+  expect_equal(colSums(newres), average_species_overlap(res))
+
 })
