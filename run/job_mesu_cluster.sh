d985c3551d6150b6ba53ea2764707dfb2bce3a64
Alain DANET <danet@mesu1.ib0.xa.dsi.upmc.fr>
2019-04-17 16:47:06 +0200

    move job exec to scratchalpha

--- run/job_mesu_cluster.sh
+++ run/job_mesu_cluster.sh
@@ -10,22 +10,44 @@
 
 # Go to my place
 cd $PBS_O_WORKDIR
-echo $pwd > loc.txt
 
-if [ -d "$HOME/bin" ] ; then
-  PATH="$PATH:$HOME/bin"
-  LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/opt/dev/Modules/Libraries"
-fi
 
 # Load R version of the cluster
 module load R/3.4.3
-## Which version is used?
-which R | echo 
-which Rscript | echo 
 
+# Load R library: 
 R_LIBS_USER="$R_LIBS_USER:$HOME/.local/share/R-3.4.3/library/"
 export R_LIBS_USER
 
+# Define scratch space scratch alpha for UV scratchbeta for ICE
+SCRATCH=/scratchalpha/$USER/my_scratch_space
+PROJECT=fishcom
+DATADIR=data
+RAWDIR=data-raw
+ANALYSIS=analysis
+PARALLEL=analysis/misc/parallel_setup.R
+
+mkdir $SCRATCH
+mkdir $SCRATCH/$PROJECT
+mkdir $SCRATCH/$PROJECT/data
+mkdir $SCRATCH/$PROJECT/data/species
+mkdir $SCRATCH/$PROJECT/data/classes
+mkdir $SCRATCH/$PROJECT/data-raw
+mkdir $SCRATCH/$PROJECT/data-raw/fishing_op_build
+mkdir $SCRATCH/$PROJECT/R
+mkdir $SCRATCH/$PROJECT/run
+mkdir $SCRATCH/$PROJECT/analysis
+mkdir $SCRATCH/$PROJECT/analysis/misc
+
+# copy some input files to  $SCRATCH directory
+cp -rp ../$DATADIR/* $SCRATCH/$PROJECT/$DATADIR
+#cp -rp ../$RAWDIR/* $SCRATCH/$PROJECT/$RAWDIR
+cp -rp ../R/* $SCRATCH/$PROJECT/R
+cp -rp ../$ANALYSIS/* $SCRATCH/$PROJECT/$ANALYSIS
+cp -rp ../run/* $SCRATCH/$PROJECT/run
+cp -rp ../DESCRIPTION $SCRATCH/$PROJECT
+
+cd $SCRATCH/$PROJECT/run
 # Launch the good guys: 
 
 #./00_dependancies.sh
@@ -37,5 +59,14 @@ wait
 wait
 # Build network classes
 ./02_run_analysis.R "PBS" "classes"
+wait
 
 echo "Is it working ?"
+
+# Copyfile
+cd $PBS_O_WORKDIR
+cp -rp  $SCRATCH/$PROJECT/$DATADIR/* ../$DATADIR || exit 1
+
+#clean the temporary directory
+rm -rf $SCRATCH/$PROJECT/*
+
