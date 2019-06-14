0a5d6246e36975ce78d617e5d52db0b9bf806a99
Alain D <alain.danet@caramail.fr>
2019-06-14 19:25:25 +0200

    Fix dependancies

--- data-raw/ssn_interpolation/04_basin_interpolation.R
+++ data-raw/ssn_interpolation/04_basin_interpolation.R
@@ -1,6 +1,11 @@
 
-library('tidyverse')
-library('magrittr')
+library(tidyverse)
+library(magrittr)
+library(sf)
+mypath <- rprojroot::find_package_root_file
+source(mypath("R", "misc.R"))
+source(mypath("R", "geo_methods.R"))
+source(mypath("R", "plot_methods.R"))
 
 #  Interpolation
 myload(quality_data, dir = mypath("data-raw"))
