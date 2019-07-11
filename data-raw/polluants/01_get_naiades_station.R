ccf8e464d57b3c9c85a32e80975d71a694e13b81
Alain D <alain.danet@caramail.fr>
2019-07-11 15:42:34 +0200

    Fix projection in naiades station

--- data-raw/polluants/01_get_naiades_station.R
+++ data-raw/polluants/01_get_naiades_station.R
@@ -8,7 +8,9 @@ library(magrittr)
 mypath <- rprojroot::find_package_root_file
 source(mypath("R", "misc.R"))
 
-data <- read_csv2(mypath("data-raw", "polluants", "naiades_data", "stations.csv"))
+data <- read_delim(
+  file = mypath("data-raw", "polluants", "naiades_data", "stations.csv"),
+  delim = ";", locale = locale(decimal_mark = "."))
 
 replacement_col <- c(
  CdStationMesureEauxSurface = "id",
@@ -30,6 +32,13 @@ data %<>%
   filter(projection == "RGF93 / Lambert 93")
 
 station_naiades <- st_as_sf(data, coords = c("lon", "lat"), crs = 2154)
+
+#Remove corsica
+myload(region_polygon, dir = mypath("data"))
+region_polygon %<>% st_transform(crs = 2154)
+int_naiades <- sf::st_intersects(station_naiades, region_polygon)
+obs_mask <- purrr::map_lgl(int_naiades, function(x) ifelse(length(x) > 0, TRUE, FALSE))
+station_naiades <- station_naiades[obs_mask, ]
 
 mysave(station_naiades, dir = mypath("data-raw"), overwrite = TRUE)
 #proj_code <- c(
