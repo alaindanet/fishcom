8adea3d4401fecc997d7e48724fc4d3e635b595b
alain danet <alain.danet@mnhn.fr>
2019-07-29 17:27:21 +0200

    Interpolation of press temperature

---
+++ data-raw/fire_ssn/temperature/00_mvg_avg.R
@@ -0,0 +1,168 @@
+################################################################################
+#                              Prepare temp data                               #
+################################################################################
+
+
+library(tidyverse)
+library(magrittr)
+library(lubridate)
+library('zoo') 
+mypath <- rprojroot::find_package_root_file
+source(mypath("R", "misc.R"))
+myload(quality_data, dir = mypath("data-raw"))
+
+
+prep_data <- quality_data %>%
+  filter(var_code == "temp") %>%
+  mutate(
+    value = as.numeric(value),
+    year_month = ymd(paste0(year(meas_date),"-", month(meas_date), "-15"))
+    ) %>%
+  group_by(id, year_month) %>%
+  summarise(value = mean(value))
+
+# Eliminate station with few records:
+few_records <- prep_data %>%
+  group_by(id) %>%
+  summarise(nobs = n()) %>%
+  filter(nobs < 24) 
+
+prep_data %<>%
+  filter(! id %in% few_records$id) %>%
+  group_by(id) %>%
+  arrange(desc(year_month)) %>%
+  nest()
+
+rm(quality_data)
+options(mc.cores = 15)
+prep_data$moving_avg <- parallel::mclapply(prep_data$data, function(x) {
+    rollapplyr(data = x$value, width = 12, FUN = mean, na.rm = TRUE, fill = NA, partial = 3)
+    })
+prep_data %<>%
+  unnest()
+prep_data %<>%
+  filter(year_month > "1994-01-01")
+
+monthly_avg_temp <- prep_data
+# Yearly avg
+mysave(monthly_avg_temp, dir = mypath("data-raw", "temp"))
+
+yearly_avg_temp <- monthly_avg_temp %>%
+  mutate(year = year(year_month)) %>%
+  group_by(id, year) %>%
+  summarise(value = mean(moving_avg, na.rm = TRUE), raw_value = mean(value, na.rm = TRUE)) %>%
+  ungroup()
+
+# To play:
+sample_monthly_avg_temp <- monthly_avg_temp %>%
+  filter(id %in% sample(monthly_avg_temp$id, 100))
+
+mysave(sample_monthly_avg_temp, monthly_avg_temp, yearly_avg_temp,
+  dir = mypath("data-raw", "temp"), overwrite = TRUE)
+
+################################################################################
+#                                Define pulses                                 #
+################################################################################
+#TODO: define low and high pulse for temp data
+
+
+myload(monthly_avg_temp,
+  press_cat, dir = mypath("data-raw", "temp"))
+mtemp <- monthly_avg_temp
+
+#################
+#  Local pulse  #
+#################
+# detrend data
+mtemp %<>%
+  mutate(value = value - moving_avg)
+# define pulse by station
+
+treshold <- 1.96 #95%
+pulse <- mtemp %>%
+  group_by(id, parameter) %>%
+  mutate(
+    z = scale(value),
+    pulse = map_lgl(z,
+      function (z) {
+	if (is.na(z)) return(NA)
+
+      }
+    )
+  )
+yearly_pulse <- pulse %>%
+  mutate(year = year(year_month)) %>%
+  group_by(id, parameter, year) %>%
+  summarise(
+    nb_pulse = sum(pulse, na.rm = TRUE),
+    pulse = any(pulse, na.rm = TRUE)
+  )
+local_yearly_pulse_temp <- yearly_pulse %>%
+  ungroup()
+
+mysave(local_yearly_pulse_temp, dir = mypath("data-raw", "temp"), overwrite = TRUE)
+
+####################
+#  National pulse  #
+####################
+
+treshold <- 1.96 #95%
+pulse <- mtemp %>%
+  group_by(parameter) %>%
+  mutate(
+    z = scale(value),
+    pulse = map2_lgl(z, direction,
+      function (z, dire) {
+	if (is.na(z)) return(NA)
+
+	if (dire == "decreasing") {
+	  if (z < treshold * -1) {
+	    return(TRUE)
+	  } else {
+	    return(FALSE)
+	  }
+	} else{
+	  if (z > treshold) {
+	    return(TRUE)
+	  } else {
+	    return(FALSE)
+	  }
+	}
+      }
+    )
+  )
+yearly_pulse <- pulse %>%
+  mutate(year = year(year_month)) %>%
+  group_by(id, parameter, year) %>%
+  summarise(
+    nb_pulse = sum(pulse, na.rm = TRUE),
+    pulse = any(pulse, na.rm = TRUE)
+  )
+global_yearly_pulse_temp <- yearly_pulse %>%
+  ungroup()
+
+mysave(global_yearly_pulse_temp, dir = mypath("data-raw", "temp"))
+
+###################################
+#  Summary data to filter interp  #
+###################################
+
+myload(monthly_avg_temp, yearly_avg_temp,
+  dir = mypath("data-raw", "temp"))
+
+dist_yearly_avg_temp <- yearly_avg_temp %>%
+  summarise(
+    mean = mean(value, na.rm = TRUE),
+    sd = sd(value, na.rm = TRUE),
+    min = min(value, na.rm = TRUE),
+    max = max(value, na.rm = TRUE))
+
+dist_monthly_avg_temp <- monthly_avg_temp %>%
+  summarise(
+    mean = mean(value, na.rm = TRUE),
+    sd = sd(value, na.rm = TRUE),
+    min = min(value, na.rm = TRUE),
+    max = max(value, na.rm = TRUE))
+
+mysave(dist_monthly_avg_temp, dist_yearly_avg_temp,
+  dir = mypath("data-raw", "temp"), overwrite = TRUE)
---
+++ data-raw/fire_ssn/temperature/01bis_save_ssn.R
@@ -0,0 +1,29 @@
+library(tidyverse)
+library(magrittr)
+mypath <- rprojroot::find_package_root_file
+mydir <- mypath("data-raw")
+source(mypath("R", "misc.R"))
+source(mypath("R", "geo_methods.R"))
+
+
+myload(my_hydro_basin, dir = mypath("data-raw", "fire_ssn"))
+
+library(parallel)
+options(mc.cores = 4)
+ssn <- mclapply(my_hydro_basin$basin_name, function(basin) {
+  pred_name <- paste0(basin, "_pred_sites")
+  ssn_dir <- mypath("data-raw", "fire_ssn", "temperature")
+  ssn_obj_path <- paste0(ssn_dir, "/", basin, ".ssn")
+  ssn <- SSN::importSSN(ssn_obj_path, predpts = pred_name, o.write = TRUE)
+# Compute the weight of each streams lines when they merged:
+  ssn <- SSN::additive.function(ssn, "H2OArea",
+    "afv_area")
+# create distance matrix between pred and obs:
+  SSN::createDistMat(ssn, predpts = pred_name, o.write = TRUE,
+    amongpreds = TRUE)
+  #SSN::writeSSN(ssn, filename = ssn_obj_path, o.write = TRUE)
+  return(ssn)
+})
+names(ssn) <- my_hydro_basin$basin_name 
+mysave(ssn, dir = mypath("data-raw", "fire_ssn", "temperature"), overwrite = TRUE)
+
