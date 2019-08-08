aa864ee3cbfa0c6d1eec4a61bb1961e2c65b8a03
Alain D <alain.danet@caramail.fr>
2019-08-08 11:56:54 +0200

    Make scripts executable

--- run/install.R
+++ run/install.R
@@ -11,7 +11,7 @@ if (is.null(argv) | length(argv) < 1) {
 repos <- "https://cran.rstudio.com"
 
 ## this makes sense on Debian where no packages touch /usr/local
-lib.loc <- "~/.local/share/R-3.4.3/library/"
+lib.loc <- "~/.local/share/R-3.6.1/library/"
 
 ## Remove from the list already installed packages
 already_installed <- installed.packages()[, "Package"]
