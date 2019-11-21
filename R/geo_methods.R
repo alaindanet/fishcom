319a34e30e49346a0b5345da5debf8b702b1c3a1
Alain D <alain.danet@caramail.fr>
2019-11-21 17:56:22 +0100

    Test stab div in different context: basin, consistence of biomass trajectories

--- R/geo_methods.R
+++ R/geo_methods.R
@@ -594,3 +594,36 @@ fill_data_ssn <- function(ssn = NULL, data = NULL, var = NULL,
 
   return(ssn)
 }
+
+#' Get hydrographic basin name for each station  
+#'
+#' @param sf_obj logical should return a spatial object
+#'
+get_basin_station <- function (sf_obj = FALSE) {
+
+  myload(the_8_hydrologic_basin, station_analysis, dir = data_common)
+
+  station_analysis %<>%
+    st_transform(crs = 2154)
+  the_8_hydrologic_basin %<>%
+    mutate(NomDistric = ifelse(CdBassinDC == "B2", "SAMBRE", NomDistric))
+
+  station_basin <- st_intersects(station_analysis, the_8_hydrologic_basin)
+  station_analysis$basin <- purrr::map_chr(station_basin, function(x){
+    if (length(x) == 0) {
+      return(NA) 
+    }
+    the_8_hydrologic_basin[["NomDistric"]][x]
+    })
+  station_analysis %<>%
+    #filter(!is.na(basin)) %>%
+    mutate(station = id) %>%
+    select(station, basin)
+
+  if (!sf_obj) {
+    st_geometry(station_analysis) <- NULL
+  }
+
+  return(station_analysis)
+
+}
